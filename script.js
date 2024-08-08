0c11bc0272d96d515125a656a8d7e883acb9eddb
Vad Colcev <vadimkolchev@gmail.com>
2024-08-09 02:10:35 +0300

    Gradual filling in element opacity implementation

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function createGrid(sideSize) {
 
     for (let i = 0; i < sideSize ** 2; i++) {
         const div = document.createElement("div");
-        div.className = "grid-element";  
+        div.className = "grid-element";
+        div.style.setProperty("opacity", constants.INITIAL_OPACITY); 
         containerDiv.appendChild(div);
         div.style.width = `${elementSizeDimensions}px`;
     }
@@ -27,13 +28,17 @@ function changeElementColor(e) {
     if (e.target.className !== "container") {
         const color = getRandomColor();
         e.target.style.backgroundColor = constructRGBColorString(color);
+        let opacity = Number(e.target.style.opacity);
+        if (opacity < 1) {
+            e.target.style.opacity = opacity + 0.1;
+        }
     }
 }
 
 function resetGrid() {
     let sideSize = 0;
 
-    while (sideSize < 2 || sideSize > 100 || isNaN(sideSize)) {
+    while (sideSize < constants.MIN_SIDE_SIZE || sideSize > constants.MAX_SIDE_SIZE || isNaN(sideSize)) {
         sideSize = Number(prompt("Enter the side size of the canvas (2 -100): "));
     }
 
@@ -50,6 +55,7 @@ const constants = {
     MAX_SIDE_SIZE: 100,
     MIN_SIDE_SIZE: 2,
     MAX_COLOR_RGB_PLUS_ONE: 257,
+    INITIAL_OPACITY: 0.1,
 }
 
 containerDiv.addEventListener("mouseover", changeElementColor);
