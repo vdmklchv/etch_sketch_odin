ff523972f6548e97fb4d7df5c30dcb78eac42055
Vad Colcev <vadimkolchev@gmail.com>
2024-08-08 23:58:15 +0300

    Create grid

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 containerDiv = document.querySelector(".container");
 
-for (let i = 0; i < 196; i++) {
+const CONTAINER_SIDE_DIMENSIONS = 960;
+const GAP_SIZE = 1;
+
+let elementsPerSide = 16;
+const pixelsPerSideWithoutGaps = CONTAINER_SIDE_DIMENSIONS - GAP_SIZE * elementsPerSide;
+const elementSizeDimensions = pixelsPerSideWithoutGaps / elementsPerSide;
+
+
+for (let i = 0; i < elementsPerSide ** 2; i++) {
     const div = document.createElement("div");
-    div.className = "grid-element";
+    div.className = "grid-element";  
     containerDiv.appendChild(div);
+    div.style.width = `${elementSizeDimensions}px`;
 }
+
+const divElements = document.querySelector(".grid-element");
