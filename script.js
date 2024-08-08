f26c029c0811137a0324255f2e2ce46dcf60801e
Vad Colcev <vadimkolchev@gmail.com>
2024-08-09 00:08:11 +0300

    Refactor

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 containerDiv = document.querySelector(".container");
 
-const CONTAINER_SIDE_DIMENSIONS = 960;
-const GAP_SIZE = 1;
-
-let elementsPerSide = 16;
-const pixelsPerSideWithoutGaps = CONTAINER_SIDE_DIMENSIONS - GAP_SIZE * elementsPerSide;
-const elementSizeDimensions = pixelsPerSideWithoutGaps / elementsPerSide;
-
-
-for (let i = 0; i < elementsPerSide ** 2; i++) {
-    const div = document.createElement("div");
-    div.className = "grid-element";  
-    containerDiv.appendChild(div);
-    div.style.width = `${elementSizeDimensions}px`;
+const constants = {
+    CONTAINER_SIDE_DIMENSIONS: 960,
+    GAP_SIZE: 1,
 }
 
-const divElements = document.querySelector(".grid-element");
+let elementsPerSide = 16;
+
+function createGrid(sideSize) {
+    console.log("here");
+    if (sideSize > 100 || sideSize < 2) return;
+
+    const pixelsPerSideWithoutGaps = constants.CONTAINER_SIDE_DIMENSIONS - constants.GAP_SIZE * sideSize;
+    const elementSizeDimensions = pixelsPerSideWithoutGaps / sideSize;
+
+    for (let i = 0; i < sideSize ** 2; i++) {
+        const div = document.createElement("div");
+        div.className = "grid-element";  
+        containerDiv.appendChild(div);
+        div.style.width = `${elementSizeDimensions}px`;
+    }
+}
+
+createGrid(elementsPerSide);
+
+
+
